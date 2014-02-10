45e713e2b059a5a4ac4798e3ea19a62ee97ef993
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-02-10 22:42:40 +0400

    add bad test

---
+++ src/test/java/demux/BadTest.java
@@ -0,0 +1,17 @@
+package demux;
+
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ */
+@Ignore
+public class BadTest {
+
+    @Test
+    public void failFast() {
+        Assert.fail("bad to the bones");
+    }
+}
