ef6aa7e957a19f26a1f4541598a13a67675bd34a
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-02-17 01:24:02 +0400

    speed test

---
+++ src/test/java/demux/SpeedTest.java
@@ -0,0 +1,15 @@
+package demux;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ */
+public class SpeedTest {
+    private static final Logger logger = LoggerFactory.getLogger(SpeedTest.class);
+    @Test
+    public void one() {
+        logger.info("one");
+    }
+}
