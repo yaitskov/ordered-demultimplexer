4ca48c14368ec908c7d4320981c293209bbf7507
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-02-22 22:23:53 +0400

    make worker interruptable and exception tolerant

--- src/main/java/com/github/dyaitskov/demux/queue1/Worker.java
+++ src/main/java/com/github/dyaitskov/demux/queue1/Worker.java
@@ -1,8 +1,12 @@
 package com.github.dyaitskov.demux.queue1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  */
 public class Worker implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(Worker.class);
     public volatile Job in;
     public volatile Job out;
     private final Processor processor;
@@ -15,9 +19,17 @@ public class Worker implements Runnable {
     public void run() {
         while (true) {
             if (in == null || out != null) {
+                if (Thread.interrupted()) {
+                    logger.info("interruption request. break cycle.");
+                    break;
+                }
                 Thread.yield();
             } else {
-                out = new Job(processor.process(in.input), in.id);
+                try {
+                    out = new Job(processor.process(in.input), in.id);
+                } catch (Throwable e) {
+                    logger.error("lost job id {}", in.id, e);
+                }
                 in = null;
             }
         }
