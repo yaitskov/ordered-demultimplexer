2e96690eb094647f94512bcd638a4f26b240ee51
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-02-22 22:15:31 +0400

    cc

--- src/main/java/com/github/dyaitskov/demux/queue1/Worker.java
+++ src/main/java/com/github/dyaitskov/demux/queue1/Worker.java
@@ -1,12 +1,8 @@
 package com.github.dyaitskov.demux.queue1;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  */
 public class Worker implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(Worker.class);
     public volatile Job in;
     public volatile Job out;
     private final Processor processor;
