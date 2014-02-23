3370b327c130505eadc283998588c0326f5b56d7
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-02-23 23:00:15 +0400

    add logging

--- src/main/java/com/github/dyaitskov/demux/queue1s/SyncBitMap.java
+++ src/main/java/com/github/dyaitskov/demux/queue1s/SyncBitMap.java
@@ -5,6 +5,8 @@ import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.Range;
 import com.google.common.collect.Sets;
 import gnu.trove.set.hash.TIntHashSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -12,6 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  */
 public class SyncBitMap {
+    private static final Logger logger = LoggerFactory.getLogger(SyncBitMap.class);
     private final int[][] combinations;
     private final AtomicInteger bits = new AtomicInteger();
     private final Object lock = new Object();
@@ -33,17 +36,21 @@ public class SyncBitMap {
 
     public int[] findSetBits() throws InterruptedException {
         int map = bits.get();
+        logger.debug("get map {}", map);
         if (map == 0) {
             synchronized (lock) {
                 while ((map = bits.get()) == 0) {
+                    logger.debug("wait bits");
                     lock.wait();
                 }
             }
         }
+        logger.debug("return map {}", map);
         return combinations[map];
     }
 
     public void clearBit(int wid) {
+        logger.debug("bit {}", wid);
         while (true) {
             int map = bits.get();
             if (bits.compareAndSet(map, map & ~(1 << wid))) {
@@ -53,11 +60,13 @@ public class SyncBitMap {
     }
 
     public void setBit(int index) {
+        logger.debug("bit {}", index);
         while (true) {
             int map = bits.get();
             if (bits.compareAndSet(map, map | (1 << index))) {
                 if (map == 0) {
                     synchronized (lock) {
+                        logger.debug("notify map not zero {}", index);
                         lock.notify();
                     }
                 }
