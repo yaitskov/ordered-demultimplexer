62b807f9ecdab5061b03e12608347342981df445
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-02-23 23:00:38 +0400

    test clearBit and setBit

--- src/test/java/com/github/dyaitskov/demux/queue1s/SyncBitMapTest.java
+++ src/test/java/com/github/dyaitskov/demux/queue1s/SyncBitMapTest.java
@@ -1,6 +1,8 @@
 package com.github.dyaitskov.demux.queue1s;
 
 
+import com.google.common.collect.Sets;
+import com.google.common.primitives.Ints;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,5 +14,19 @@ public class SyncBitMapTest {
     public void constructor() {
         SyncBitMap map = new SyncBitMap(8);
         Assert.assertNotNull(map);
+    }
+
+    @Test
+    public void setClear() throws InterruptedException {
+        SyncBitMap map = new SyncBitMap(8);
+        map.setBit(0);
+        map.setBit(1);
+        Assert.assertEquals(
+                Sets.newHashSet(Ints.asList(map.findSetBits())),
+                Sets.newHashSet(0, 1));
+        map.clearBit(0);
+        Assert.assertEquals(
+                Sets.newHashSet(Ints.asList(map.findSetBits())),
+                Sets.newHashSet(1));
     }
 }
