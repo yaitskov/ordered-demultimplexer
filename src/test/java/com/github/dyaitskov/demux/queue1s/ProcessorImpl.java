b53db962ae0052315fd8a33a69557179841aaa0b
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2014-02-23 23:32:09 +0400

    randomize processor load

--- src/test/java/com/github/dyaitskov/demux/queue1s/ProcessorImpl.java
+++ src/test/java/com/github/dyaitskov/demux/queue1s/ProcessorImpl.java
@@ -3,12 +3,20 @@ package com.github.dyaitskov.demux.queue1s;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 /**
 */
 public class ProcessorImpl implements Processor {
     private static final Logger logger = LoggerFactory.getLogger(SourceImpl.class);
     public Object process(Object input) {
-        logger.debug("processor {}", input);
+        Random r = new Random();
+        long l = 0;
+        int m = r.nextInt((Integer) input + 10);
+        for (int i = 0; i < m; ++i) {
+            l += r.nextInt(10);
+        }
+        logger.debug("processor {}, {}", input, l);
         return - ((Integer) input);
     }
 }
